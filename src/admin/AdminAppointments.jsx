04f5b4599e2f10ef86f65633775de05c9e2f6895
sld12345 <sld.dk123123@gmail.com>
2025-04-11 20:49:37 +0530

    Update AdminAppointments.jsx

--- src/admin/AdminAppointments.jsx
+++ src/admin/AdminAppointments.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import "./adminAppointments.css";
+import "./AdminAppointments.css";
 
 const AdminAppointments = () => {
   const [appointments, setAppointments] = useState([]);
